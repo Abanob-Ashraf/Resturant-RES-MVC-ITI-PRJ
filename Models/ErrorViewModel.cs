5b00a06075325493b1108dd85ad21c707a9469fe
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-19 21:28:30 +0300

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
