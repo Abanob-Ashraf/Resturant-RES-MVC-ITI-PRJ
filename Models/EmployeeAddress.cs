8a37a38e66c09f4653b018f776408c29d93aabec
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-21 15:34:57 +0300

    Create EmployeeAddress Model

---
+++ Models/EmployeeAddress.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    [Table("EmployeeAddress")]
+    public class EmployeeAddress
+    {
+        [Key]
+        public int AddressId { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [ForeignKey("Employee")]
+        public int EmpId { get; set; }
+
+        public Employee? Employee { get; set; }
+    }
+}
