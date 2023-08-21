d1d8344f035fd286e9bfa6c04ba62601ab504517
Nader Sayed <nadersayed001@gmail.com>
2023-08-21 20:47:49 +0300

    [Create] Reservation Tables Relation Model

---
+++ Areas/Client/Models/ReservationTablesRel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
+{
+    [Table("ReservationTablesRel")]
+    public class ReservationTablesRel
+    {
+        [Key]
+        public int ReservationTablesRelID { get; set; }
+
+        [ForeignKey("Reservation")]
+        public int ReservationID { get; set; }
+
+        [ForeignKey("Tables")]
+        public int TableID { get; set; }
+
+
+        public Reservations Reservation { get; set; }
+
+        public Tables Tables { get; set; }
+    }
+}
