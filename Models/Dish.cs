96bc291e4f92e2becbbfe60e8465edbcee502afb
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-21 17:35:12 +0300

    [Create] Dishes Model

---
+++ Models/Dish.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    [Table("Dish")]
+
+    public class Dish
+    {
+        [Key]
+        public int DishId { get; set; }
+
+        [Required]
+        public double DishPrice { get; set; }
+
+        [Required]
+        public string DishImageName { get; set; }
+
+
+        [ForeignKey("DishesCategories")]
+        public int DishCategoryId { get; set; }
+
+        public DishesCategories? DishesCategories { get; set; }
+
+    }
+}
