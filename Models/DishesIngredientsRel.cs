e016ae556998c13869b7c511424fe6705c5b60cd
ziad osama <ziadosama1234@gmail.com>
2023-08-21 16:11:13 +0300

    [Create] DishesIngredientsRel Model

---
+++ Models/DishesIngredientsRel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    [Table("DishesIngredientsRel")]
+    public class DishesIngredientsRel
+    {
+        [Key]
+        [Required]
+        public int DishesIngredientsRelId { get; set; }
+
+        [Required]
+        [ForeignKey("Dishes")]
+        public int DishId { get; set; }
+
+        [Required]
+        [ForeignKey("Ingredtients")]
+        public  int  IngerdientId { get; set; }
+
+        public  Dishes? Dishes { get; set; }
+        
+        public   Ingerdients? Ingredtients { get; set; }
+    }
+}
