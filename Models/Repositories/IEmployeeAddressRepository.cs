26125ee39b3d0bbdce49ffb7c9174dee9697ad5c
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 01:32:14 +0300

    [Create] Employee Addresses Repository

---
+++ Models/Repositories/IEmployeeAddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories
+{
+    public interface IEmployeeAddressRepository
+    {
+
+        public List<EmployeeAddress> GetAllEmployeesAddress();
+        public EmployeeAddress GetEmployeeAddressById(int id);
+        public void InsertEmployeeAddress(EmployeeAddress employee);
+        public void UpdateEmployeeAddress(EmployeeAddress employee);
+        public void DeleteEmployeeAddress(int id);
+    }
+}
