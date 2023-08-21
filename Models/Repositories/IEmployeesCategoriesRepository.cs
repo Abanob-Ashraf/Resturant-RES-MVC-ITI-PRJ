3e8bb6f2fe65e40f94cc3e7ebb2221a2f088b6ba
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 01:38:29 +0300

    [Create] Employees Category Repository

---
+++ Models/Repositories/IEmployeesCategoriesRepository.cs
@@ -0,0 +1,18 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories
+{
+    public interface IEmployeesCategoriesRepository
+    {
+
+        public List<EmployeesCategories> GetAllEmployeesCategories();
+
+        public EmployeesCategories GetEmployeesCategoryById(int id);
+
+        public void InsertEmployeesCategory(EmployeesCategories employeesCategories);
+
+        public void UpdateEmployeesCategory(EmployeesCategories employeesCategories);
+
+        public void DeleteEmployeesCategory(int id);
+    }
+}
