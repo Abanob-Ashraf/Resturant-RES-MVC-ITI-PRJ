4af3f4956120fb83bed0f86ce03ea10f468a3dd6
Nader Sayed <nadersayed001@gmail.com>
2023-08-21 19:54:09 +0300

    [Update] Reservaions Model

--- Areas/Client/Models/Reservations.cs
+++ Areas/Client/Models/Reservations.cs
@@ -1,9 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Resturant_RES_MVC_ITI_PRJ.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
 {
     [Table("Reservations")]
     public class Reservations
     {
+        [Key]
+        public int ReservationID { get; set; }
+
+        [CustomMinDate]
+        public DateTime ReservarionDate { get; set; }
+        [MaxLength]
+        public string Notes { get; set; }
+
+        [ForeignKey("Customers")]
+        public int CustomerID { get; set; }
+
+        [ForeignKey("Table")]
+        public int TableID { get; set; }
+        public Customers Customers { get; set; }
+        public Tables Table { get; set; }
+
     }
 }
