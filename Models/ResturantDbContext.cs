2b3a75dcacc27402a96afe88bf620b3dee89907f
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-21 23:25:39 +0300

    [Create] DbContext

---
+++ Models/ResturantDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    public class ResturantDbContext : IdentityDbContext
+    {
+
+    }
+}
