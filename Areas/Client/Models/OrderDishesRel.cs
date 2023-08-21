d2168304d8b5562ea754cb9dc994c7f74cf54bcc
Mina Alpert <minaalpert1212@gmail.com>
2023-08-21 18:46:29 +0300

    [Create] OrderDishesRel Model

--- Areas/Client/Models/OrderDishesRel.cs
+++ Areas/Client/Models/OrderDishesRel.cs
@@ -5,5 +5,23 @@ namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
     [Table("OrderDishesRel")]
     public class OrderDishesRel
     {
+
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        public int Quantity { get; set; }
+
+
+
+        [ForeignKey("Order")]
+        public int OrderId { get; set; }
+        public Order? Order { get; set; }
+
+
+        [ForeignKey("Dish")]
+        public int DishId { get; set; }
+        public Dish? Dish { get; set; }
     }
 }
