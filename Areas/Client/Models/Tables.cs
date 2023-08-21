0cac01ad5cbc18dc0cff9dc7c960ce37892d2674
Nader Sayed <nadersayed001@gmail.com>
2023-08-21 19:27:11 +0300

    [Update] Tables Model

--- Areas/Client/Models/Tables.cs
+++ Areas/Client/Models/Tables.cs
@@ -1,9 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
 {
     [Table("Tables")]
     public class Tables
     {
+        [Key]
+        public int TablesID { get; set; }
+
+        [Required(ErrorMessage = "Enter Table Number")]
+        public string TableNumber { get; set; }
+
+
+        public int ChairsNumbers { get; set; }
+
+
+        public bool isReservered { get; set; }
     }
 }
