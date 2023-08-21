57dfca5b22e327a80c9ca1f8979e86caf93f1aab
Nader Sayed <nadersayed001@gmail.com>
2023-08-21 18:56:48 +0300

    [Update] Testimonials Model

--- Areas/Client/Models/Testimonials.cs
+++ Areas/Client/Models/Testimonials.cs
@@ -1,9 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
 {
     [Table("Testimonials")]
     public class Testimonials
     {
+        [Key]
+        public int TestimonialsID { get; set; }
+
+        [MaxLength]
+        public int TestimonialsText { get; set; }
+
+        [ForeignKey("Customers")]
+        public int CustomerID { get; set; }
+
+        public Customers Customers { get; set; }
+
     }
 }
