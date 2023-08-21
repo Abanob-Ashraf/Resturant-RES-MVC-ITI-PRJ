5b517fb15ec70662b78ec0192c7f3c202cd1f49a
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-21 23:26:37 +0300

    [Create] DbContext

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Resturant_RES_MVC_ITI_PRJ.Models;
+
 namespace Resturant_RES_MVC_ITI_PRJ
 {
     public class Program
@@ -9,6 +12,9 @@ namespace Resturant_RES_MVC_ITI_PRJ
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddDbContext<ResturantDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            //builder.Services.Configure<MinDateOptions>(builder.Configuration.GetSection("MinimumDate"));
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -18,6 +24,7 @@ namespace Resturant_RES_MVC_ITI_PRJ
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
