08b827445e3c8708f90bec516ffd83d24c86be6e
Mina Alpert <minaalpert1212@gmail.com>
2023-08-21 18:42:42 +0300

    [Create] Order Model

--- Areas/Client/Models/Order.cs
+++ Areas/Client/Models/Order.cs
@@ -5,5 +5,38 @@ namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
     [Table("Order")]
     public class Order
     {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "You Must Enter Order")]
+        [StringLength(50)]
+        public string OrderState { get; set; }
+
+
+        [Required]
+        public string PaymentMethod { get; set; }
+
+        [Required]
+        public bool IsPaid { get; set; }
+
+
+
+
+        [ForeignKey("OrderType")]
+        public int OrderTypeId { get; set; }
+        public OrderType? OrderType { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+
+
+        [ForeignKey("Franchise")]
+        public int FranchiseId { get; set; }
+        public Franchise? Franchise { get; set; }
+
+
+
+        public ICollection<OrderDishesRel>? OrderDishesRels { get; set; }
     }
 }
