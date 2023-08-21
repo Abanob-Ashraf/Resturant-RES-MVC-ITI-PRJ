248809dc8881619979a105ce56f92a96b7336ff1
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 01:14:28 +0300

    [Create] Employee Repository

---
+++ Models/Repositories/Interface.cs
@@ -0,0 +1,17 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        public List<Employee> GetAllEmployees();
+
+        public Employee GetEmployeeById(int id);
+
+        public void InsertEmployee(Employee employee);
+
+        public void UpdateEmployee(Employee employee);
+
+        public void DeleteEmployee(int id);
+    }
+}
