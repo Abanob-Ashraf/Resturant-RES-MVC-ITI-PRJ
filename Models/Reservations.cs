e26c4fccad04a3b58348de1dbe6fdacfda8146e8
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-21 16:45:44 +0300

    [Update] Customers Model

--- Models/Reservations.cs
+++ Models/Reservations.cs
@@ -1,5 +1,8 @@
-﻿namespace Resturant_RES_MVC_ITI_PRJ.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
 {
+    [Table("Reservations")]
     public class Reservations
     {
     }
