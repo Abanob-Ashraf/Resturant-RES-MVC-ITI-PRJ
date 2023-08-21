160c66fa179c560eeb9613bda6e34ee165af8a75
ziad osama <ziadosama1234@gmail.com>
2023-08-21 16:10:25 +0300

    [Create] Dishes Model

---
+++ Models/Dishes.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    [Table("Dishe")]
+
+    public class Dishes
+    {
+        
+
+        [Key]
+        public int DishesId { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public string ImageName { get; set; }
+
+
+        [ForeignKey("DishesCategories")]
+        public int DishCategoryId { get; set; }
+
+        public DishesCategories? DishesCategories { get; set; }
+
+    }
+}
