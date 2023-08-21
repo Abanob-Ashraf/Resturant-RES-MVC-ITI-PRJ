c55ea09ec7d2cbb7196a31449e036686b1356c5f
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-21 15:35:23 +0300

    Create EmployeesCategories Model

---
+++ Models/EmployeesCategories.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    [Table("EmployeesCategories")]
+    public class EmployeesCategories
+    {
+        [Key]
+        public int EmployeesCategoriesId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
