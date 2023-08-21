c6834be090c3304e30d5cfa0459c5196d6c98df4
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-21 15:35:59 +0300

    Create Franchise Model

---
+++ Models/Franchise.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    [Table("Franchise")]
+    public class Franchise
+    {
+        //  id[pk]
+        //Street string
+        //City string
+        //Country string
+        //MangerID int
+
+        [Key]
+        public int FranchiseId { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [ForeignKey("Manager")]
+        public int ManagerId { get; set; }
+        public Employee? Manager { get; set; }
+
+        [InverseProperty("Franchise")]
+        public ICollection<Employee>? Employees { get; set; }
+
+
+    }
+}
