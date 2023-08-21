498d1c8dbef1999cfe653d40eb7ca0c45f31de8b
ziad osama <ziadosama1234@gmail.com>
2023-08-21 16:10:45 +0300

    [Create] DishesCategories Model

---
+++ Models/DishesCategories.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    [Table("DishesCategorie")]
+
+    public class DishesCategories
+    {
+        [Key]
+      public  int DishesCategoriesId { get; set; }
+
+        [Required]
+     public   string DishCategoryName { get; set; }
+
+        public ICollection<Dishes>? Dishes { get; set; }
+    }
+}
