06243047197bf754644b2968ff0d4ad29da6cb40
ziad osama <ziadosama1234@gmail.com>
2023-08-21 16:12:00 +0300

    [Create] Ingredients Model

---
+++ Models/Ingerdients.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    [Table("Ingredient")]
+    public class Ingerdients
+    {
+        [Key]
+       
+        public int IngerdientsId { get; set; }
+        [Required]
+        public string IngName { get; set; }
+
+
+      public virtual ICollection<DishesIngredientsRel>? DishesIngredientsRel { get; set; }
+    }
+}
