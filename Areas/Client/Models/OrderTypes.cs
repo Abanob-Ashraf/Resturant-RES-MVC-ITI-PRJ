120db2efc6285a9d0c47ee1228e369af4e99f20a
Mina Alpert <minaalpert1212@gmail.com>
2023-08-21 18:46:56 +0300

    [Create] OrderTypes Model

--- Areas/Client/Models/OrderTypes.cs
+++ Areas/Client/Models/OrderTypes.cs
@@ -5,5 +5,16 @@ namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
     [Table("OrderTypes")]
     public class OrderTypes
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You Must Enter Order")]
+        [StringLength(50)]
+        public string OrderTypeName { get; set; }
+
+
+
+        public ICollection<Order>? Orders { get; set; }
     }
 }
