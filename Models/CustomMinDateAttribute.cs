ab1c2cff9a4e4a85a9d53844606f18c8c0689009
Nader Sayed <nadersayed001@gmail.com>
2023-08-21 19:47:45 +0300

    [Create] Custom Date Data Annotation

---
+++ Models/CustomMinDateAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    public class CustomMinDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime dateValue)
+            {
+                return dateValue >= DateTime.Today;
+            }
+            return false;
+        }
+    }
+}
