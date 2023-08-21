fd0cd8b5028b148e17d31472b1cfbd512b12a077
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 02:17:19 +0300

    [Restrcutre] Adding Management & client Folder

---
+++ Models/Repositories/Management/IFranchiseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories
+{
+    public interface IFranchiseRepository
+    {
+
+        public List<Franchise> GetAllFranchises();
+
+        public Franchise GetFranchiseById(int id);
+
+        public void InsertFranchise(Franchise franchise);
+
+        public void UpdateFranchise(Franchise franchise);
+
+        public void DeleteFranchise(int id);
+    }
+}
