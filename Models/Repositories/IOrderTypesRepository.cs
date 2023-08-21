dd8ec831bd93cd6fc20637417a1bdb79be7b3d2c
ziad osama <ziadosama1234@gmail.com>
2023-08-22 01:38:56 +0300

    [Create] Order Types Repository

---
+++ Models/Repositories/IOrderTypesRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories
+{
+
+    public interface IOrderTypesRepository
+    {
+        public List<OrderTypes> GetAllOrderTypes();
+
+        public OrderTypes GetOrderTypesById(int id);
+
+        public void InsertOrderTypes(OrderTypes orderTypes);
+
+        public void UpdateOrderTypes(int id, OrderTypes orderTypes);
+
+        public void DeleteOrderTypes(int id);
+    }
+}
