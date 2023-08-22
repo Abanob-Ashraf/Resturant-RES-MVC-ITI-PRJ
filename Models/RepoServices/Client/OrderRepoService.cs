fb43485a762a697402570fc7884d2ff76d85281c
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 13:13:49 +0300

    [Create] Order Repository Service

---
+++ Models/RepoServices/Client/OrderRepoService.cs
@@ -0,0 +1,33 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class OrderRepoService : IOrderRepository
+    {
+        public List<Franchise> GetAllOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Franchise GetOrderById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOrder(Franchise franchise)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrder(Franchise franchise)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
