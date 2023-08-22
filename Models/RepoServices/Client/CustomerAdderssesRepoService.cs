c286d87b5231c02dedd904c6c161ff16404e7849
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 12:47:29 +0300

    [Create] Customer Addersses Repository

---
+++ Models/RepoServices/Client/CustomerAdderssesRepoService.cs
@@ -0,0 +1,35 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class CustomerAdderssesRepoService : ICustomerAdderssesRepository
+    {
+
+        public List<Franchise> GetAllCustomerAddersses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Franchise GetCustomerAdderssById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertCustomerAdderss(Franchise franchise)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomerAdderss(Franchise franchise)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCustomerAdderss(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
