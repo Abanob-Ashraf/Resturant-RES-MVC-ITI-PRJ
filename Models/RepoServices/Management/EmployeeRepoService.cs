4d05a417de7eb91d2ab903c29d9a591ce49e3088
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 13:01:09 +0300

    [Create] Employees Repository Service

---
+++ Models/RepoServices/Management/EmployeeRepoService.cs
@@ -0,0 +1,33 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Management
+{
+    public class EmployeeRepoService : IEmployeeRepository
+    {
+        public List<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
