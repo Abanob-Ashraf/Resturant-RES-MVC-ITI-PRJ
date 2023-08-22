faabad4f779ce411d52cad2bff25a3d2b7c655ed
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-22 04:00:51 +0300

    [Rename] Following Naming Convention

---
+++ Models/Repositories/Client/IOrderesDishesRelRepository.cs
@@ -0,0 +1,17 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client
+{
+    public interface IOrderesDishesRelRepository
+    {
+        public List<OrderesDishesRel> GetAllOrderDishesRels();
+
+        public OrderesDishesRel GetOrderDishesRelById(int id);
+
+        public void InsertOrderDishesRel(OrderesDishesRel orderDishesRel);
+
+        public void UpdateOrderDishesRel(OrderesDishesRel orderDishesRel);
+
+        public void DeleteOrderDishesRel(int id);
+    }
+}
