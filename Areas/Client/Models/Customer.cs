0dc4b09237ca1a857c6503c6e1347eaedfcbb553
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 17:43:22 +0300

    [Modify] Display names for columns

--- Areas/Client/Models/Customer.cs
+++ Areas/Client/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -14,6 +15,7 @@ namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
         public int CustID { get; set; }
         [Required(ErrorMessage = "You Must Enter Customer Name")]
         [StringLength(50)]
+        [DisplayName("Customer Name")]
         public string CustName { get; set; }
 
         [Required(ErrorMessage = "You Must Enter Email")]
@@ -22,11 +24,13 @@ namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
 
         [Required(ErrorMessage = "You Must Enter Email")]
         [DataType(DataType.Password)]
+        [DisplayName("Email")]
         [RegularExpression("\"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$\"\r\n", ErrorMessage = "Enter a strong passwor")]
         public string CustPassword { get; set; }
 
         [RegularExpression(@"^(010|011|012|015)\d{8}$", ErrorMessage = "start with 010 | 011 | 012 | 015 and max 11 Diget")]
         [MaxLength(11)]
+        [DisplayName("Phone Number")]
         public string CustPhone { get; set; }
 
         [InverseProperty("Customer")]
