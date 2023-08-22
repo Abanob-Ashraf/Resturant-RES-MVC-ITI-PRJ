e3486100f4011048315c1cc37d123e2ae8c96a71
ziad osama <ziadosama1234@gmail.com>
2023-08-22 23:46:27 +0300

    [Create] ingredient repository Service

--- Models/RepoServices/Client/IngerdientRepoService.cs
+++ Models/RepoServices/Client/IngerdientRepoService.cs
@@ -1,4 +1,5 @@
-﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
 using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
@@ -6,30 +7,48 @@ namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
     public class IngerdientRepoService : IIngerdientRepository
     {
 
+        public ResturantDbContext Ctx { get; }
+
+        public IngerdientRepoService(ResturantDbContext ctx)
+        {
+            Ctx = ctx;
+        }
 
         public List<Ingerdient> GetAllIngerdients()
         {
-            throw new NotImplementedException();
+            return Ctx.Ingerdients
+                 .Include(Ingerdient => Ingerdient.DishIngredientRels)
+                 .ToList();
         }
 
         public Ingerdient GetIngerdientById(int id)
         {
-            throw new NotImplementedException();
+
+            if (id == 0)
+            {
+                throw new ArgumentException($"Can't Find That Dish with Id: {id}");
+            }
+            return Ctx.Ingerdients
+                .Include(Ingerdient => Ingerdient.DishIngredientRels)
+               .Where(Ingerdient => Ingerdient.IngerdientId == id).SingleOrDefault();
         }
 
         public void InsertIngerdient(Ingerdient Ingerdient)
         {
-            throw new NotImplementedException();
+            Ctx.Ingerdients.Add(Ingerdient);
+            Ctx.SaveChanges();
         }
 
         public void UpdateIngerdient(int id, Ingerdient Ingerdient)
         {
-            throw new NotImplementedException();
+            Ctx.Ingerdients.Update(Ingerdient);
+            Ctx.SaveChanges();
         }
 
         public void DeleteIngerdient(int id)
         {
-            throw new NotImplementedException();
+            Ctx.Ingerdients.Remove(GetIngerdientById(id));
+            Ctx.SaveChanges();
         }
     }
 }
