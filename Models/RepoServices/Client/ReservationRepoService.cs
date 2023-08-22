dc6a0077da1116b3534b4f25f0cd440fe40782c6
ziad osama <ziadosama1234@gmail.com>
2023-08-22 20:48:54 +0300

    [Create] Reservation Repository Serveice

--- Models/RepoServices/Client/ReservationRepoService.cs
+++ Models/RepoServices/Client/ReservationRepoService.cs
@@ -1,33 +1,61 @@
-﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
 using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
 {
     public class ReservationRepoService : IReservationRepository
     {
+        public ResturantDbContext Ctx { get; }
+
+        public ReservationRepoService(ResturantDbContext ctx)
+        {
+            Ctx = ctx;
+        }
+
+
         public List<Reservation> GetAllReservations()
         {
-            throw new NotImplementedException();
+            return Ctx.Reservations
+                  .Include(Res => Res.Customer)
+                  .Include(Res => Res.Table)
+                  .ToList();
         }
 
         public Reservation GetReservationById(int id)
         {
-            throw new NotImplementedException();
+            if (id == 0)
+            {
+                throw new ArgumentException($"Can't Find That Reservation with Id: {id}");
+            }
+            return Ctx.Reservations
+                   .Include(Res => Res.Customer)
+                   .Include(Res => Res.Table)
+                   .Where(Res => Res.ReservationID == id).SingleOrDefault();
         }
 
         public void InsertReservation(Reservation Reservation)
         {
-            throw new NotImplementedException();
+            if (Reservation != null)
+            {
+                Ctx.Reservations.Add(Reservation);
+                Ctx.SaveChanges();
+            }
         }
 
         public void UpdateReservation(int id, Reservation Reservation)
         {
-            throw new NotImplementedException();
+            if (Reservation != null)
+            {
+                Ctx.Reservations.Update(Reservation);
+                Ctx.SaveChanges();
+            }
         }
 
         public void DeleteReservation(int id)
         {
-            throw new NotImplementedException();
+            Ctx.Reservations.Remove(GetReservationById(id));
+            Ctx.SaveChanges();
         }
     }
 }
