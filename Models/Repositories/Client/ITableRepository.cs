fcdb79303fafe239dc052c0aae8721b5247cbb8d
ziad osama <ziadosama1234@gmail.com>
2023-08-22 03:26:29 +0300

    [create] Table Repository

---
+++ Models/Repositories/Client/ITableRepository.cs
@@ -0,0 +1,18 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client
+{
+
+    public interface ITableRepository
+    {
+        public List<Table> GetAllTables();
+
+        public Table GetTablesById(int id);
+
+        public void InsertTable(Table Table);
+
+        public void UpdateTable(int id, Table Table);
+
+        public void DeleteTable(int id);
+    }
+}
