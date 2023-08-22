909cf854a8cf2947e4db7ad35a6114f409384d16
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-22 15:48:48 +0300

    [Implement] Employee Repository Service Methods

--- Models/RepoServices/Management/EmployeeRepoService.cs
+++ Models/RepoServices/Management/EmployeeRepoService.cs
@@ -1,33 +1,65 @@
-﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
 using Resturant_RES_MVC_ITI_PRJ.Models.Repositories;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Management
 {
     public class EmployeeRepoService : IEmployeeRepository
     {
+        public ResturantDbContext Ctx { get; }
+
+        public EmployeeRepoService(ResturantDbContext ctx)
+        {
+            Ctx = ctx;
+        }
+
         public List<Employee> GetAllEmployees()
         {
-            throw new NotImplementedException();
+            return Ctx.Employees
+                .Include(emp => emp.EmployeeAddress)
+                .Include(emp => emp.EmployeeCategory)
+                .Include(emp => emp.Franchise)
+                .Include(emp => emp.Franchises).ToList();
+                
         }
 
         public Employee GetEmployeeById(int id)
         {
-            throw new NotImplementedException();
+            if(id == 0)
+            {
+                throw new ArgumentException($"Can't Find That Employee with Id: {id}");
+            }
+            return Ctx.Employees
+                .Include (emp => emp.EmployeeAddress)
+                .Include (emp => emp.EmployeeCategory)
+                .Include (emp => emp.Franchise)
+                .Include (emp => emp.Franchises)
+                .Where(emp => emp.EmpID == id).SingleOrDefault();
         }
 
         public void InsertEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            if (employee != null)
+            {
+                Ctx.Employees.Add(employee);
+                Ctx.SaveChanges();
+            }
         }
 
         public void UpdateEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            if (employee != null)
+            {
+                Ctx.Employees.Update(employee);
+                Ctx.SaveChanges();
+            }
         }
 
         public void DeleteEmployee(int id)
         {
-            throw new NotImplementedException();
+            Ctx.Employees.Remove(GetEmployeeById(id));
+            Ctx.SaveChanges();
         }
     }
 }
