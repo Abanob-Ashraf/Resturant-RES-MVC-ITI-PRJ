ef410765814ec8645c1b848294bba5c270e48844
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 13:16:58 +0300

    [Create] Table Repository Service

---
+++ Models/RepoServices/Client/TableRepoService.cs
@@ -0,0 +1,35 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class TableRepoService : ITableRepository
+    {
+
+
+        public List<Table> GetAllTables()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Table GetTableById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertTable(Table Table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTable(int id, Table Table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTable(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
