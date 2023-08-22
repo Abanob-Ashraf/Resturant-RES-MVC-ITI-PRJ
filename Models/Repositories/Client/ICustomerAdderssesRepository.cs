f725a0245c1cb06e57915a141fe881ccbf32ed01
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 03:22:44 +0300

    [Create] Customer Address

---
+++ Models/Repositories/Client/ICustomerAdderssesRepository.cs
@@ -0,0 +1,17 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client
+{
+    public interface ICustomerAdderssesRepository
+    {
+        public List<Franchise> GetAllCustomerAddersses();
+
+        public Franchise GetCustomerAdderssById(int id);
+
+        public void InsertCustomerAdderss(Franchise franchise);
+
+        public void UpdateCustomerAdderss(Franchise franchise);
+
+        public void DeleteCustomerAdderss(int id);
+    }
+}
