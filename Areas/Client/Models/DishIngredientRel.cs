7a5e97f355253eb121da6827f389382c4563f593
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-22 03:51:48 +0300

    [Rename] Following Naming Convention

---
+++ Areas/Client/Models/DishIngredientRel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models
+{
+    [Table("DishIngredientRel")]
+    public class DishIngredientRel
+    {
+        [Key]
+        [Required]
+        public int DishIngredientRelId { get; set; }
+
+        [Required]
+        [ForeignKey("Dish")]
+        public int DishId { get; set; }
+        public Dish? Dish { get; set; }
+
+        [Required]
+        [ForeignKey("Ingredtient")]
+        public int IngerdientId { get; set; }
+        public Ingerdient? Ingredtient { get; set; }
+    }
+}
