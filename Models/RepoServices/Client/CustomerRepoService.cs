f0a2eed0804cd31991fe7663564ea5a4abd08586
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 14:28:27 +0300

    [Create] GetAllCustomers Method

--- Models/RepoServices/Client/CustomerRepoService.cs
+++ Models/RepoServices/Client/CustomerRepoService.cs
@@ -1,15 +1,24 @@
-﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
 using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
 {
     public class CustomerRepoService : ICustomerRepository
     {
+        public ResturantDbContext Ctx { get; }
 
-
+        public CustomerRepoService(ResturantDbContext ctx)
+        {
+            Ctx = ctx;
+        }
         public List<Customer> GetAllCustomers()
         {
-            throw new NotImplementedException();
+            return Ctx.Customers
+                 .Include(emp => emp.Testimonials)
+                 .Include(emp => emp.Reservations)
+                 .Include(emp => emp.CustomerAddersses)
+                 .Include(emp => emp.Orders).ToList();
         }
 
         public Customer GetCustomerById(int id)
