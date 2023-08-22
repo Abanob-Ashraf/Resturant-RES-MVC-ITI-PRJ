636064a339bd2e35d8d924e021bf0479887c5ad4
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 12:46:26 +0300

    [Create] Customer Repository Service

---
+++ Models/RepoServices/Client/CustomerRepoService.cs
@@ -0,0 +1,36 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class CustomerRepoService : ICustomerRepository
+    {
+
+
+        public List<Customer> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
