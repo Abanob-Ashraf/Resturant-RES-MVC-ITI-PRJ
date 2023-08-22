2f606f7cf0519eb21bfdc1207306cf4c387e0b7a
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 12:55:04 +0300

    [Create] Ingerdient Repository Service

---
+++ Models/RepoServices/Client/IngerdientRepoService.cs
@@ -0,0 +1,35 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class IngerdientRepoService : IIngerdientRepository
+    {
+
+
+        public List<Ingerdient> GetAllIngerdients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ingerdient GetIngerdientById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertIngerdient(Ingerdient Ingerdient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateIngerdient(int id, Ingerdient Ingerdient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteIngerdient(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
