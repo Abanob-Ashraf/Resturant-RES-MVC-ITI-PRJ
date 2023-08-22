15c736bb15eb592d9111346c56586c8b7f5a133b
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 13:15:59 +0300

    [Create] Reservation Repository Service

---
+++ Models/RepoServices/Client/ReservationRepoService.cs
@@ -0,0 +1,33 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class ReservationRepoService : IReservationRepository
+    {
+        public List<Reservation> GetAllReservations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation GetReservationById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertReservation(Reservation Reservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateReservation(int id, Reservation Reservation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteReservation(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
