c374aa992f656e66ee4d1b38482c0a9d80706ee1
ziad osama <ziadosama1234@gmail.com>
2023-08-23 00:08:22 +0300

    [Modify] Ingredient repository Service

--- Models/RepoServices/Client/IngerdientRepoService.cs
+++ Models/RepoServices/Client/IngerdientRepoService.cs
@@ -26,7 +26,7 @@ namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
 
             if (id == 0)
             {
-                throw new ArgumentException($"Can't Find That Dish with Id: {id}");
+                throw new ArgumentException($"Can't Find That Ingerdient with Id: {id}");
             }
             return Ctx.Ingerdients
                 .Include(Ingerdient => Ingerdient.DishIngredientRels)
@@ -35,14 +35,20 @@ namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
 
         public void InsertIngerdient(Ingerdient Ingerdient)
         {
-            Ctx.Ingerdients.Add(Ingerdient);
-            Ctx.SaveChanges();
+            if (Ingerdient != null)
+            {
+                Ctx.Ingerdients.Add(Ingerdient);
+                Ctx.SaveChanges();
+            }
         }
 
         public void UpdateIngerdient(int id, Ingerdient Ingerdient)
         {
-            Ctx.Ingerdients.Update(Ingerdient);
-            Ctx.SaveChanges();
+            if (Ingerdient != null)
+            {
+                Ctx.Ingerdients.Update(Ingerdient);
+                Ctx.SaveChanges();
+            }
         }
 
         public void DeleteIngerdient(int id)
