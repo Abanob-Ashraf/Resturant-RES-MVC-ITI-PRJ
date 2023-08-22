3e753fe7762097c2e806a59a8663cf83d79969bd
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 13:14:44 +0300

    [Create] Order Types Repository Service

---
+++ Models/RepoServices/Client/OrderTypesRepoService.cs
@@ -0,0 +1,35 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class OrderTypesRepoService : IOrderTypesRepository
+    {
+
+
+        public List<OrderType> GetAllOrderTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderType GetOrderTypeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOrderType(OrderType orderTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderType(int id, OrderType orderTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrderType(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
