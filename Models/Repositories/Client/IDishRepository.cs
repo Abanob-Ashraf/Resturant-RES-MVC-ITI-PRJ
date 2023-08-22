e4d20e8e1955cec0628a4af3fe5831cf4d3382ed
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-22 03:56:20 +0300

    [Create] Dish Repository

---
+++ Models/Repositories/Client/IDishRepository.cs
@@ -0,0 +1,17 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client
+{
+    public interface IDishRepository
+    {
+        public List<Dish> GetAllDishes();
+
+        public Dish GetDishById(int id);
+
+        public void InsertDish(Dish dish);
+
+        public void UpdateDish(Dish dish);
+
+        public void DeleteDish(int id);
+    }
+}
