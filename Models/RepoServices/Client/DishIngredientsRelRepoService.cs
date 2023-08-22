6ebec7a58dd4db65a172d461e5687c915cefc26c
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 12:53:49 +0300

    [Create] Dish Ingredients Relation Repostory Service

---
+++ Models/RepoServices/Client/DishIngredientsRelRepoService.cs
@@ -0,0 +1,36 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class DishIngredientsRelRepoService : IDishIngredientRelRepository
+    {
+
+
+        public List<DishIngredientRel> GetAllDishIngredientRels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dish GetDishIngredientRelById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertDishIngredientRel(DishIngredientRel dishIngredientRel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDishIngredientRel(DishIngredientRel dishIngredientRel)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void DeleteDishIngredientRel(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
