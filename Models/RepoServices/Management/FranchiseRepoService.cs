68c38c5d7672625e71a09c7579ae6d761b81cb17
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 13:06:35 +0300

    [Create] Franchise Repository Service

---
+++ Models/RepoServices/Management/FranchiseRepoService.cs
@@ -0,0 +1,35 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Management
+{
+    public class FranchiseRepoService : IFranchiseRepository
+    {
+
+
+        public List<Franchise> GetAllFranchises()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Franchise GetFranchiseById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertFranchise(Franchise franchise)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFranchise(Franchise franchise)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFranchise(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
