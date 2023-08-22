6300ef2a80d1f7e19ffabdc62674f857df441eb8
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 15:45:29 +0300

    [Implement] Index Action

--- Areas/Client/Controllers/CustomerController.cs
+++ Areas/Client/Controllers/CustomerController.cs
@@ -4,7 +4,7 @@ using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Controllers
 {
-
+    [Route("Customers")]
     public class CustomerController : Controller
     {
         public ICustomerRepository CustomerRepository { get; }
@@ -13,10 +13,10 @@ namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Controllers
         {
             CustomerRepository = customerRepository;
         }
-
+        [Route("GetAllCustomers")]
         public ActionResult Index()
         {
-            return View();
+            return View("Index", CustomerRepository.GetAllCustomers());
         }
 
         public ActionResult Details(int id)
