749146643da9bd832cb595fcfcd4676cf4587d93
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 13:20:39 +0300

    [Create] Testimonials Repository Service

---
+++ Models/RepoServices/Client/TestimonialsRepoService.cs
@@ -0,0 +1,35 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class TestimonialsRepoService : ITestimonialsRepository
+    {
+
+
+        public List<Testimonials> GetAllTestimonials()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Testimonials GetTestimonialsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertTestimonials(Testimonials Testimonials)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTestimonials(int id, Testimonials Testimonials)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTestimonials(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
