315ba781bd96ae460aba754a4fa47cfb01b65299
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 13:12:02 +0300

    [Create] Orderes Dishes Relatione Repository Service

---
+++ Models/RepoServices/Client/OrderesDishesRelRepoService.cs
@@ -0,0 +1,35 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class OrderesDishesRelRepoService : IOrderesDishesRelRepository
+    {
+
+
+        public List<OrderesDishesRel> GetAllOrderDishesRels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderesDishesRel GetOrderDishesRelById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOrderDishesRel(OrderesDishesRel orderDishesRel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderDishesRel(OrderesDishesRel orderDishesRel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrderDishesRel(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
