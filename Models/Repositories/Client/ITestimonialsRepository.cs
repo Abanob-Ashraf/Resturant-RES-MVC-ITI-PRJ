cb98bdd7159cca67118ee0c0d133b436b9419c6f
ziad osama <ziadosama1234@gmail.com>
2023-08-22 03:40:20 +0300

    [create] Testimonials Repository

---
+++ Models/Repositories/Client/ITestimonialsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client
+{
+    public interface ITestimonialsRepository
+    {
+        public List<Testimonials> GetAllTestimonials();
+
+        public Testimonials GetTestimonialsById(int id);
+
+        public void InsertTestimonials(Testimonials Testimonials);
+
+        public void UpdateTestimonials(int id, Testimonials Testimonials);
+
+        public void DeleteTestimonials(int id);
+
+    }
+}
