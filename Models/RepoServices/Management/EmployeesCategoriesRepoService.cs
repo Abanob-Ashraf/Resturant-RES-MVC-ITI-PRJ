126b2cbff1b0e06c277bb0285ca766301fafcac0
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-22 16:56:47 +0300

    [Implement] Employee Category Repository Service Methods

--- Models/RepoServices/Management/EmployeesCategoriesRepoService.cs
+++ Models/RepoServices/Management/EmployeesCategoriesRepoService.cs
@@ -1,35 +1,57 @@
-﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
 using Resturant_RES_MVC_ITI_PRJ.Models.Repositories;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Management
 {
     public class EmployeesCategoriesRepoService : IEmployeesCategoriesRepository
     {
+        public ResturantDbContext Ctx { get; }
 
+        public EmployeesCategoriesRepoService(ResturantDbContext ctx)
+        {
+            Ctx = ctx;
+        }
 
         public List<EmployeeCategory> GetAllEmployeesCategories()
         {
-            throw new NotImplementedException();
+            return Ctx.EmployeeCategories
+                .Include(emp => emp.Employees).ToList();
         }
 
         public EmployeeCategory GetEmployeesCategoryById(int id)
         {
-            throw new NotImplementedException();
+            if (id == 0)
+            {
+                throw new ArgumentException($"Can't Find That EmployeeCategory with Id: {id}");
+            }
+            return Ctx.EmployeeCategories
+                .Include(emp => emp.Employees)
+                .Where(emp => emp.EmployeeCategoryId == id).SingleOrDefault();
         }
 
         public void InsertEmployeesCategory(EmployeeCategory employeesCategories)
         {
-            throw new NotImplementedException();
+            if (employeesCategories != null)
+            {
+                Ctx.EmployeeCategories.Add(employeesCategories);
+                Ctx.SaveChanges();
+            }
         }
 
         public void UpdateEmployeesCategory(EmployeeCategory employeesCategories)
         {
-            throw new NotImplementedException();
+            if (employeesCategories != null)
+            {
+                Ctx.EmployeeCategories.Update(employeesCategories);
+                Ctx.SaveChanges();
+            }
         }
 
         public void DeleteEmployeesCategory(int id)
         {
-            throw new NotImplementedException();
+            Ctx.EmployeeCategories.Remove(GetEmployeesCategoryById(id));
+            Ctx.SaveChanges();
         }
     }
 }
