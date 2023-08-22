945b149ad8af7ac6771a055e74012486afc2672e
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-22 03:54:13 +0300

    [Create] Dish Category Repository

---
+++ Models/Repositories/Client/IDishCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client
+{
+    public interface IDishCategoryRepository
+    {
+        public List<DishCategory> GetAllDishCategories();
+
+        public DishCategory GetDishCategoryById(int id);
+
+        public void InsertDishCategory(DishCategory dishCategory);
+
+        public void UpdateDishCategory(DishCategory dishCategory);
+
+        public void DeleteDishCategory(int id);
+    }
+}
