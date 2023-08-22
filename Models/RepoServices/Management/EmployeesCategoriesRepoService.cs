cc46a7daf2f503c1cbb66b8f82b8de6b4bc73dec
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 13:05:15 +0300

    [Create] Employees Categories Repository Service

---
+++ Models/RepoServices/Management/EmployeesCategoriesRepoService.cs
@@ -0,0 +1,35 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Management
+{
+    public class EmployeesCategoriesRepoService : IEmployeesCategoriesRepository
+    {
+
+
+        public List<EmployeeCategory> GetAllEmployeesCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeCategory GetEmployeesCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertEmployeesCategory(EmployeeCategory employeesCategories)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployeesCategory(EmployeeCategory employeesCategories)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployeesCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
