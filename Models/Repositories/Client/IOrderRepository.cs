552a29ea073a887922fbb2823313caa04a2c0cd9
ziad osama <ziadosama1234@gmail.com>
2023-08-22 21:23:03 +0300

    [Modify] Order repository

--- Models/Repositories/Client/IOrderRepository.cs
+++ Models/Repositories/Client/IOrderRepository.cs
@@ -1,16 +1,17 @@
-﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client
 {
     public interface IOrderRepository
     {
-        public List<Franchise> GetAllOrders();
+        public List<Order> GetAllOrders();
 
-        public Franchise GetOrderById(int id);
+        public Order GetOrderById(int id);
 
-        public void InsertOrder(Franchise franchise);
+        public void InsertOrder(Order Order);
 
-        public void UpdateOrder(Franchise franchise);
+        public void UpdateOrder(Order Order);
 
         public void DeleteOrder(int id);
     }
