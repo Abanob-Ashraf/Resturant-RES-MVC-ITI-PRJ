a888ec3fa314e110ca52ed7f5e37825bdb456939
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-22 03:55:55 +0300

    [Create] Dish Ingredient Relation Repository

---
+++ Models/Repositories/Client/IDishIngredientsRelRepository.cs
@@ -0,0 +1,17 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client
+{
+    public interface IDishIngredientRelRepository
+    {
+        public List<DishIngredientRel> GetAllDishIngredientRels();
+
+        public Dish GetDishIngredientRelById(int id);
+
+        public void InsertDishIngredientRel(DishIngredientRel dishIngredientRel);
+
+        public void UpdateDishIngredientRel(DishIngredientRel dishIngredientRel);
+
+        public void DeleteDishIngredientRel(int id);
+    }
+}
