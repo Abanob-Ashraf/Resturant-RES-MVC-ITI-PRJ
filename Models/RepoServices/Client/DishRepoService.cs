bb34331f2d2e32e59eb425a4dd51f46f47c45e98
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 12:49:01 +0300

    [Create] Dish Repository Service

---
+++ Models/RepoServices/Client/DishRepoService.cs
@@ -0,0 +1,33 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+{
+    public class DishRepoService : IDishRepository
+    {
+        public List<Dish> GetAllDishes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dish GetDishById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertDish(Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDish(Dish dish)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDish(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
