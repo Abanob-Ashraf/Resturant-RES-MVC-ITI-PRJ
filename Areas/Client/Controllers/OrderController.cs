52af9a8586e1ca08ed9a0433a9864b4b3bb7b99e
ziad osama <ziadosama1234@gmail.com>
2023-08-22 23:32:33 +0300

    [Create] Order controller

---
+++ Areas/Client/Controllers/OrderController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class OrderController : Controller
+    {
+        public IOrderRepository OrderRepository { get; }
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            OrderRepository = orderRepository;
+        }
+        //[Route("/GetAllTables")]
+        public ActionResult Index()
+        {
+            return View("Index", OrderRepository.GetAllOrders());
+        }
+
+        // GET: OrderController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OrderController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrderController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OrderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrderController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OrderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
