c2e361518d199bdb00381354fefcf1f3546551b8
Nader Sayed <nadersayed001@gmail.com>
2023-08-22 12:52:15 +0300

    [Update] Implement Dish Category Repo Interface

--- Models/RepoServices/Client/DishCategoryRepoService.cs
+++ Models/RepoServices/Client/DishCategoryRepoService.cs
@@ -1,6 +1,35 @@
-﻿namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Client
 {
-    public class DishCategoryRepoService
+    public class DishCategoryRepoService : IDishCategoryRepository
     {
+
+
+        public List<DishCategory> GetAllDishCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DishCategory GetDishCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertDishCategory(DishCategory dishCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDishCategory(DishCategory dishCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDishCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
