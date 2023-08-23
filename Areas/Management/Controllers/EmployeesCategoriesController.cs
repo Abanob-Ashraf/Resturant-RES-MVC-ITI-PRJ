036d0949bdd41cdf7d2a638752c23f93acd4358c
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-23 17:48:21 +0300

    [Implement] Employees Categories Controller methods

--- Areas/Management/Controllers/EmployeesCategoriesController.cs
+++ Areas/Management/Controllers/EmployeesCategoriesController.cs
@@ -1,61 +1,76 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Areas.Management.Controllers
 {
+    [Area("Management")]
+    //[Route("EmployeesCategory")]
     public class EmployeesCategoriesController : Controller
     {
+        public IEmployeesCategoriesRepository EmployeesCategoriesRepository { get; }
+
+        public EmployeesCategoriesController(IEmployeesCategoriesRepository employeesCategoriesRepository)
+        {
+            EmployeesCategoriesRepository = employeesCategoriesRepository;
+        }
+
+        //[Route("GetAllCategories")]
         public ActionResult Index()
         {
-            return View();
+            return View("Index", EmployeesCategoriesRepository.GetAllEmployeesCategories());
         }
 
+        //[Route("GetCategoriesById/{id:int}")]
         public ActionResult Details(int id)
         {
-            return View();
+            return View("Details", EmployeesCategoriesRepository.GetEmployeesCategoryById(id));
         }
 
+        //[Route("CreateEmployeeCategory")]
         public ActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
+        //[Route("CreateEmployeeCategory")]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(EmployeeCategory employeeCategory)
         {
-            try
+            if (ModelState.IsValid)
             {
+                EmployeesCategoriesRepository.InsertEmployeesCategory(employeeCategory);
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
-        }
-
-        public ActionResult Edit(int id)
-        {
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        //[Route("UpdateEmployeeCategory")]
+        public ActionResult Edit(int id)
         {
-            try
+            return View(EmployeesCategoriesRepository.GetEmployeesCategoryById(id));
+
+        }
+
+        [HttpPost]
+        //[Route("UpdateEmployeeCategory")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EmployeeCategory employeeCategory)
+        {
+            if (ModelState.IsValid)
             {
+                EmployeesCategoriesRepository.UpdateEmployeesCategory(employeeCategory);
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
 
         public ActionResult Delete(int id)
         {
-            return View();
+            EmployeesCategoriesRepository.DeleteEmployeesCategory(id);
+            return RedirectToAction(nameof(Index));
         }
 
         //[HttpPost]
