b561f1eba47d38d37bcf5e01a9fecb1140784ad4
Mina Alpert <minaalpert1212@gmail.com>
2023-08-24 02:09:59 +0300

    [Update] IOrdersDishesRelRepository

--- Models/Repositories/Client/IOrdersDishesRelRepository.cs
+++ Models/Repositories/Client/IOrdersDishesRelRepository.cs
@@ -4,13 +4,13 @@ namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client
 {
     public interface IOrdersDishesRelRepository
     {
-        public List<OrderesDishesRel> GetAllOrderDishesRels();
+        public List<OrderDishesRel> GetAllOrderDishesRels();
 
-        public OrderesDishesRel GetOrderDishesRelById(int id);
+        public OrderDishesRel GetOrderDishesRelById(int id);
 
-        public void InsertOrderDishesRel(OrderesDishesRel orderDishesRel);
+        public void InsertOrderDishesRel(OrderDishesRel orderDishesRel);
 
-        public void UpdateOrderDishesRel(OrderesDishesRel orderDishesRel);
+        public void UpdateOrderDishesRel(int id, OrderDishesRel orderDishesRel);
 
         public void DeleteOrderDishesRel(int id);
     }
