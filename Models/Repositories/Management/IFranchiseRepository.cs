aa4483b65bac8c2c669e039cbd8edfc95c6184b3
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-23 18:49:23 +0300

    [Update] Error Handlers

--- Models/Repositories/Management/IFranchiseRepository.cs
+++ Models/Repositories/Management/IFranchiseRepository.cs
@@ -11,7 +11,7 @@ namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories
 
         public void InsertFranchise(Franchise franchise);
 
-        public void UpdateFranchise(Franchise franchise);
+        public void UpdateFranchise(int id, Franchise franchise);
 
         public void DeleteFranchise(int id);
     }
