d6ff3e5c4dc0991e6dbb19b2020b715cae4e00aa
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-23 15:12:28 +0300

    [Impelement] Franchise RepoService methods

--- Models/RepoServices/Management/FranchiseRepoService.cs
+++ Models/RepoServices/Management/FranchiseRepoService.cs
@@ -1,35 +1,61 @@
-﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
 using Resturant_RES_MVC_ITI_PRJ.Models.Repositories;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Management
 {
     public class FranchiseRepoService : IFranchiseRepository
     {
+        public ResturantDbContext Ctx { get; }
 
+        public FranchiseRepoService(ResturantDbContext ctx)
+        {
+            Ctx = ctx;
+        }
 
         public List<Franchise> GetAllFranchises()
         {
-            throw new NotImplementedException();
+            return Ctx.Franchises
+                .Include(emp => emp.Manager)
+                .Include(emp => emp.Employees)
+                .Include(emp => emp.Orders).ToList();
         }
 
         public Franchise GetFranchiseById(int id)
         {
-            throw new NotImplementedException();
+            if (id == 0)
+            {
+                throw new ArgumentException($"Can't Find That Franchise with Id: {id}");
+            }
+            return Ctx.Franchises
+                .Include(emp => emp.Manager)
+                .Include(emp => emp.Employees)
+                .Include(emp => emp.Orders)
+                .Where(emp => emp.FranchiseId == id).SingleOrDefault();
         }
 
         public void InsertFranchise(Franchise franchise)
         {
-            throw new NotImplementedException();
+            if (franchise != null)
+            {
+                Ctx.Franchises.Add(franchise);
+                Ctx.SaveChanges();
+            }
         }
 
         public void UpdateFranchise(Franchise franchise)
         {
-            throw new NotImplementedException();
+            if (franchise != null)
+            {
+                Ctx.Franchises.Update(franchise);
+                Ctx.SaveChanges();
+            }
         }
 
         public void DeleteFranchise(int id)
         {
-            throw new NotImplementedException();
+            Ctx.Franchises.Remove(GetFranchiseById(id));
+            Ctx.SaveChanges();
         }
     }
 }
