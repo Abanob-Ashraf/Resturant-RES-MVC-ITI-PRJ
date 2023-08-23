019ee1f82b577b0ce3e9b117ec0f63d12ebb781e
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-23 15:13:18 +0300

    [Create] Custom Date Range Attribute

---
+++ Models/DateRangeAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    public class DateRangeAttribute : ValidationAttribute
+    {
+        DateTime Minimum;
+        DateTime Maximum;
+
+        public DateRangeAttribute(string dateTime)
+        {
+            Minimum = DateTime.Parse(dateTime);
+            Maximum = DateTime.Today.AddDays(15);
+        }
+
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime dateValue)
+            {
+                if (dateValue >= Minimum && dateValue <= Maximum)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    ErrorMessage = $"Min Value {Minimum} and Max Value {Maximum}";
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+            return new ValidationResult("The value must be a valid date.");
+        }
+    }
+}
