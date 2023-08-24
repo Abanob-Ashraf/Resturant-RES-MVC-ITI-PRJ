8b4f50ad6bc177cf4a9f5d1dc7d09c3fc1610cc3
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-24 07:55:05 +0300

    [Impelement] Customer controller methods

--- Areas/Client/Controllers/CustomerController.cs
+++ Areas/Client/Controllers/CustomerController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
+using Resturant_RES_MVC_ITI_PRJ.Models.Repositories;
 using Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Client;
 
 namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Controllers
@@ -13,71 +16,74 @@ namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Controllers
         {
             CustomerRepository = customerRepository;
         }
+
+        //[Route("GetAllCustomers")]
         public ActionResult Index()
         {
             return View("Index", CustomerRepository.GetAllCustomers());
         }
 
+        //[Route("GetCustomerById/{id:int}")]
         public ActionResult Details(int id)
         {
-            return View(CustomerRepository.GetCustomerById(id));
+            return View("Details", CustomerRepository.GetCustomerById(id));
         }
 
+        //[Route("CreateCustomer")]
         public ActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
+        //[Route("CreateCustomer")]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Customer customer)
         {
-            try
+            if (ModelState.IsValid)
             {
+                CustomerRepository.InsertCustomer(customer);
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
 
+        //[Route("EditCustomer")]
         public ActionResult Edit(int id)
         {
-            return View();
+            return View(CustomerRepository.GetCustomerById(id));
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Customer customer)
         {
-            try
+            if (ModelState.IsValid)
             {
+                CustomerRepository.UpdateCustomer(customer);
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
 
         public ActionResult Delete(int id)
         {
-            return View();
+            CustomerRepository.DeleteCustomer(id);
+            return RedirectToAction(nameof(Index));
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
