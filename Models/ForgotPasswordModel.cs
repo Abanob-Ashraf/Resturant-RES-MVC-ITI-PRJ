d5708884353f8c07e613ddea63a4b9d665bab6c8
ziad osama <ziadosama1234@gmail.com>
2023-08-26 01:44:32 +0300

    [Create] Reset Password Models

---
+++ Models/ForgotPasswordModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models
+{
+    public class ForgotPasswordModel
+    {
+
+    
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+        
+    }
+}
