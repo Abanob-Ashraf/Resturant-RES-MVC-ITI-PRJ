8b7a99af513eac567fd12543b9290b85f64d10dc
ziad osama <ziadosama1234@gmail.com>
2023-08-26 01:46:25 +0300

    [Create & Implement ] Email Controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,6 +6,9 @@ using Microsoft.Identity.Client;
 using Resturant_RES_MVC_ITI_PRJ.Models;
 using System.Security.Claims;
 using Resturant_RES_MVC_ITI_PRJ.Models.ViewModels;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using Resturant_RES_MVC_ITI_PRJ.Services;
+using Message = Resturant_RES_MVC_ITI_PRJ.Services.Message;
 
 namespace WebAppday8.Controllers
 {
@@ -13,13 +16,14 @@ namespace WebAppday8.Controllers
     {
         private readonly UserManager<AppUser> userManager;
         private readonly SignInManager<AppUser> signInManager;
-
+        private readonly IEmailSender emailSender;
         //public ITraineerepo Traineerepo { get; }
 
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IEmailSender emailSender)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
+            this.emailSender = emailSender;
             //this.Traineerepo = traineerepo;
         }
 
@@ -185,6 +189,76 @@ namespace WebAppday8.Controllers
             var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
             return Challenge(properties, provider);
         }
+
+
+
+
+        //---------------------------------------------forget password
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordModel forgotPasswordModel)
+        {
+            if (!ModelState.IsValid)
+                return View(forgotPasswordModel);
+            var user = await userManager.FindByEmailAsync(forgotPasswordModel.Email);
+            if (user == null)
+                return RedirectToAction(nameof(ForgotPasswordConfirmation));
+            var token = await userManager.GeneratePasswordResetTokenAsync(user);
+         
+            var callback = Url.Action(nameof(ResetPassword), "Account", new { token, email = user.Email }, Request.Scheme);
+
+            var message = new Message(new string[] { user.Email }, "Reset your password at ZMAN Restaurants", callback, null);
+            await emailSender.SendEmailAsync(message);
+            return RedirectToAction(nameof(ForgotPasswordConfirmation));
+        }
+
+        public IActionResult ForgotPasswordConfirmation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword(string token, string email)
+        {
+            var model = new ResetPasswordModel { Token = token, Email = email };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ResetPassword(ResetPasswordModel resetPasswordModel)
+        {
+            if (!ModelState.IsValid)
+                return View(resetPasswordModel);
+            var user = await userManager.FindByEmailAsync(resetPasswordModel.Email);
+            if (user == null)
+                RedirectToAction(nameof(ResetPasswordConfirmation));
+            var resetPassResult = await userManager.ResetPasswordAsync(user, resetPasswordModel.Token, resetPasswordModel.Password);
+            if (!resetPassResult.Succeeded)
+            {
+                foreach (var error in resetPassResult.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+                return View();
+            }
+            return RedirectToAction(nameof(ResetPasswordConfirmation));
+
+        }
+
+        [HttpGet]
+        public IActionResult ResetPasswordConfirmation()
+        {
+            return View();
+        }
+
 
 
     }
