3d8392137cc533147c77860e3fb57853e207781c
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-27 02:04:16 +0300

    [Implment] Order Analysis

--- Models/RepoServices/Analysis/OrderAnalysis.cs
+++ Models/RepoServices/Analysis/OrderAnalysis.cs
@@ -13,26 +13,32 @@ namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Analysis
             Ctx = ctx;
         }
 
+        //done
         public int GetNoCustomerOrdered()
         {
-            var NoCustomerOrdered = Ctx.Orders.Where(c => c.OrderDate == DateTime.Now).Select(c => c.CustomerId).Count();
+            var NoCustomerOrdered = Ctx.Orders.Where(c => c.OrderDate == DateTime.Today).Select(c => c.CustomerId).Count();
             Console.WriteLine(NoCustomerOrdered);
             return NoCustomerOrdered;
         }
+
+
         public List<Dish> LeastOrderedDishes()
         {
-            throw new NotImplementedException();
+            var data = Ctx.OrderesDishesRels.OrderByDescending(c => c.Order.OrderDate >= DateTime.Today).Select(c => c.Dish).ToList();
+            return data;
         }
 
+        //done
         public List<Customer> GetMostCustomersOrderedMoreOne()
         {
             var MostCustomers = Ctx.Orders
                 .GroupBy(c => c.CustomerId)
-                .Where(c => c.Count() > 1)
+                .Where(c => c.Count() > 1).OrderByDescending(c => c.Count())
                 .Select(c => c.First().Customer).ToList();
             return MostCustomers;
         }
 
+        //done
         public Dictionary<Dish, int> GetMostOrderedDishes()
         {
             var MostDishes = Ctx.OrderesDishesRels
@@ -41,37 +47,16 @@ namespace Resturant_RES_MVC_ITI_PRJ.Models.RepoServices.Analysis
                 {
                     Dish = Ctx.Dishes.FirstOrDefault(v => v.DishId == c.Key),
                     Quantity = c.Sum(v => v.Quantity),
-                }).ToDictionary(b => b.Dish, b => b.Quantity);
-                return MostDishes;
+                }).OrderByDescending(c => c.Quantity).ToDictionary(c => c.Dish, c => c.Quantity);
+            return MostDishes;
         }
 
-        //public List<Dish> LeastOrderedDishes()
-        //{
-        //    var data;
-        //    return data;
-        //}
-        //public List<Customer> GetCustomersOrdered()
-        //{
-        //    var MostCustomers = Ctx.Orders
-        //        .GroupBy(c => c.CustomerId)
-        //        .Where(c => c.Count() > 1)
-        //        .Select(c => c.First().Customer).ToList();
-        //    return MostCustomers;
-        //}
-
+        //done
         public Dictionary<string, List<Order>> GetOrderByType()
         {
-            var orders = Ctx.Orders;
-            var OrderByType = new Dictionary<string, List<Order>>();
-
-            foreach (var order in orders)
-            {
-                if (!OrderByType.ContainsKey(order.OrderType.OrderTypeName))
-                {
-                    OrderByType[order.OrderType.OrderTypeName] = new List<Order>();
-                }
-                OrderByType[order.OrderType.OrderTypeName].Add(order);
-            }
+            var OrderByType = Ctx.Orders
+                .GroupBy(c => c.OrderType.OrderTypeName)
+                .ToDictionary(c => c.Key, c => c.ToList());
             return OrderByType;
         }
     }
