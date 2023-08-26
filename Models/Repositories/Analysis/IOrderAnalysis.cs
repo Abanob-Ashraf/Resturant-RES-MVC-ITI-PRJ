efff63fd21f9993a3d5284012e75b61ce6472d57
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-08-26 09:09:33 +0300

    [Create & Implement] Order Analysis

---
+++ Models/Repositories/Analysis/IOrderAnalysis.cs
@@ -0,0 +1,11 @@
+﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models;
+
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Analysis
+{
+    public interface IOrderAnalysis
+    {
+        public int GetNoCustomerOrdered();
+
+        public Dictionary<string, List<Order>> GetOrderByType();
+    }
+}
