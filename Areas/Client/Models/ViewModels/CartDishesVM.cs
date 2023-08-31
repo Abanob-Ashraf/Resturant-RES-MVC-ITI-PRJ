fa9baed77ff3b3888c05ba709d90bbf2643c7319
Nader Sayed <nadersayed001@gmail.com>
2023-08-31 03:20:16 +0300

    [Create] Cart View Models According to frontend needings

---
+++ Areas/Client/Models/ViewModels/CartDishesVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models.ViewModels
+{
+    public class CartDishesVM
+    {
+        public int DishId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Areas/Client/Models/ViewModels/CartVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Resturant_RES_MVC_ITI_PRJ.Areas.Client.Models.ViewModels
+{
+	public class CartVM
+	{
+		public int PaymentMethodID { get; set; }
+		public List<CartDishesVM> cartDishes { get; set; }
+	}
+}
