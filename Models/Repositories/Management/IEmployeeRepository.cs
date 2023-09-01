e2a87d584d4e3c4a71aa240d621550dec06c0fa3
Nader Sayed <nadersayed001@gmail.com>
2023-09-01 21:13:34 +0300

    [Modify] Namespace path

--- Models/Repositories/Management/IEmployeeRepository.cs
+++ Models/Repositories/Management/IEmployeeRepository.cs
@@ -1,6 +1,6 @@
 ﻿using Resturant_RES_MVC_ITI_PRJ.Areas.Management.Models;
 
-namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories.Management
+namespace Resturant_RES_MVC_ITI_PRJ.Models.Repositories
 {
     public interface IEmployeeRepository
     {
